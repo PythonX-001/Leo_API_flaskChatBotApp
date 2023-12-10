3a8001f9d9b355b2c37884f2b0bdd30043aeefb0
No0ne003 <hi.yassir.op@gmail.com>
2023-12-10 17:34:50 +0100

    Correcting my Name on template/index.html and app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ leo = Leo()
 def askleo(prompt):
 #max_tokens_to_sample is an int : 400 to 2048
     # You can use it as Llama by explicitly leaving System Prompt empty.
-    system_prompt = 'you are an ai named none0 your job is to help devolopers in solving theit problems and and help them .rules : your response should be complete and well explained , if someone asked you somthing that dont have a relation with coding and programming dont answer his question and  say " im a coding ai expert i cannot help you"'
+    system_prompt = 'you are an ai named No0ne your job is to help devolopers in solving theit problems and and help them .rules : your response should be complete and well explained , if someone asked you somthing that dont have a relation with coding and programming dont answer his question and  say " im a coding ai expert i cannot help you"'
     response = leo.ask(prompt, system_prompt=system_prompt,max_tokens_to_sample=2048)
     response = response.completion
     print(response)
